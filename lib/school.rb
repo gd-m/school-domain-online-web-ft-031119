679886443154429367cdbd4991f4e68599a7c639
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-21 01:33:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,11 +1,13 @@
 # code here!
 class School
-  attr_accessor :school_name
+  attr_accessor :school_name, :add_student
 
   roster = {}
 
   def initialize(school_name)
     @school_name =school_name
   end
+
+  
 
 end
